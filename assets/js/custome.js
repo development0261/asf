dec56e279371fdbc543988123d59d0ed07ec710b
Logix Built <demo.logixbuiltinfo@gmail.com>
2021-10-29 19:32:29 +0530

    first commit

---
+++ assets/js/custome.js
@@ -0,0 +1,20 @@
+$('.quote-s').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    dots: true,
+    slideBy: 2,
+    pullDrag: true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
